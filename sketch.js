c3f1494710571e01d1ba1f85643312643f2a2c86
Gustavo Diel <gustavodiel@hotmail.com>
2018-05-04 23:34:48 -0300

    Working. Only black-white

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 let curr = [];
 let prev = [];
 
-const SIZE_X = 200
-const SIZE_Y = 200
+const SIZE_X = 800
+const SIZE_Y = 800
 
-const DAMPING = 0.9
+const DAMPING = 0.95
 
 function setup() {
 	createCanvas( SIZE_X, SIZE_Y );
@@ -16,16 +16,22 @@ function setup() {
 			prev[ i ][ j ] = 0
 		}
 	}
+
+	curr[ 100 ][ 100 ] = 255
 }
 
 function swapBuffers() {
-	for ( var i = 0; i < SIZE_X; i++ ) {
-		for ( var j = 0; j < SIZE_Y; j++ ) {
-			let buffer = curr[ i ][ j ]
-			curr[ i ][ j ] = prev[ i ][ j ]
-			prev[ i ][ j ] = buffer
-		}
-	}
+	let temp = curr
+	curr = prev
+	prev = temp
+}
+
+function mouseClicked() {
+	prev[ mouseX ][ mouseY ] = 255
+}
+
+function mouseDragged() {
+	prev[ mouseX ][ mouseY ] = 255
 }
 
 function draw() {
@@ -33,12 +39,14 @@ function draw() {
 	loadPixels();
 	for ( let i = 1; i < SIZE_X - 1; i++ ) {
 		for ( let j = 1; j < SIZE_Y - 1; j++ ) {
-			curr[ i ][ j ] = (
-				prev[ i - 1 ][ j ] + prev[ i + 1 ][ j ] +
-				prev[ i ][ j + 1 ] + prev[ i ][ j - 1 ] ) / 2 - curr[ i ][ j ];
+			curr[ i ][ j ] = ( prev[ i - 1 ][ j ] + prev[ i + 1 ][ j ] + prev[ i ][ j + 1 ] + prev[ i ][ j - 1 ] ) / 2 - curr[ i ][ j ];
+			curr[ i ][ j ] = curr[ i ][ j ] * DAMPING
 
-			let index = i + j * SIZE_Y;
-			pixels[ index ] = color( curr[ i ][ j ] )
+			var index = 4 * ( i + j * SIZE_Y );
+			pixels[ index + 0 ] = curr[ i ][ j ]
+			pixels[ index + 1 ] = curr[ i ][ j ]
+			pixels[ index + 2 ] = curr[ i ][ j ]
+			pixels[ index + 3 ] = 255
 		}
 	}
 	updatePixels();
