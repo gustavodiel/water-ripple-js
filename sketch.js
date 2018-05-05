9fe7a9ad993b356bae5ab3978eb9ecd406b4c7e6
Gustavo Diel <gustavodiel@hotmail.com>
2018-05-05 11:09:18 -0300

    Final

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let curr = [];
 let prev = [];
 
-const SIZE_X = 1000
-const SIZE_Y = 1000
+const SIZE_X = 600
+const SIZE_Y = 600
 
 const DAMPING = 0.95
 
@@ -13,8 +13,8 @@ function preload() {}
 
 function setup() {
 	createCanvas( SIZE_X, SIZE_Y );
-	waves = createImage( SIZE_X, SIZE_Y )
 
+	waves = createImage( SIZE_X, SIZE_Y )
 	backgroundImg = loadImage( "assets/Water.jpg" );
 
 	for ( var i = 0; i < SIZE_X; i++ ) {
@@ -42,24 +42,18 @@ function mouseDragged() {
 }
 
 function draw() {
-	background( 0 );
 	image( backgroundImg, 0, 0, width, height )
 	waves.loadPixels();
 	for ( let i = 1; i < SIZE_X - 1; i++ ) {
 		for ( let j = 1; j < SIZE_Y - 1; j++ ) {
 			curr[ i ][ j ] = ( prev[ i - 1 ][ j ] + prev[ i + 1 ][ j ] + prev[ i ][ j + 1 ] + prev[ i ][ j - 1 ] ) / 2 - curr[ i ][ j ];
 			curr[ i ][ j ] = curr[ i ][ j ] * DAMPING
-
-			// var index = 4 * ( i + j * SIZE_Y );
-			// pixels[ index + 0 ] += curr[ i ][ j ]
-			// pixels[ index + 1 ] += curr[ i ][ j ]
-			// pixels[ index + 2 ] += curr[ i ][ j ]
-			waves.set( i, j, [ curr[ i ][ j ], curr[ i ][ j ], curr[ i ][ j ], 100 ] )
-
+			waves.set( i, j, [ curr[ i ][ j ], curr[ i ][ j ], curr[ i ][ j ], 205 ] )
 		}
 	}
+	prev[ floor( random( SIZE_X ) ) ][ floor( random( SIZE_Y ) ) ] = 10000
 	waves.updatePixels();
-	blend( waves, 0, 0, width, height, 0, 0, width, height, ADD );
+	blend( waves, 0, 0, width, height, 0, 0, width, height, SOFT_LIGHT );
 	// filter( BLUR, 3 );
 	swapBuffers();
 }
